2cea990b6156ddd7eb31dff74d07090d6e362e44
Anna Loginova <space@Annas-MacBook-Air.local>
2018-09-23 23:53:20 +0200

    added complex tests

--- src/roboCleaner.spec.ts
+++ src/roboCleaner.spec.ts
@@ -17,6 +17,13 @@ test('RoboCleaner overlays', () =>{
     expect(roboCleaner(2, [10, 10], ['N 5', 'S 3'])).toBe(6);
 });
 
+
+test("RoboCleaner complex", () =>{
+    expect(roboCleaner(9, [10, 10], ['N 3', 'E 4', 'S 2', 'W 2', 'N 2', 'E 3', 'N 1', 'W 4', 'S 2'])).toBe(20);
+    expect(roboCleaner(9, [100, 100], 'W5 N1 E6 S2 W2 N4 W2 S2 E6'.split(' ').map(el => el[0] + ' ' + el.substr(1)))).toBe(24)
+    expect(roboCleaner(28, [100, 100], 'S4 E1 N2 W5 N1 E1 S2 W2 N1 W3 N1 E1 S2 W4 N3 E2 S4 W1 N2 W4 N1 E1 S3 W2 N4 W1 S2 E14'.split(' ').map(el => el[0] + ' ' + el.substr(1)))).toBe(54);
+});
+
 test('PathsIntersect', () => {
     expect(pathsIntersect(new Path([0, 0], [10, 0]), new Path([0, 0], [0, 10]))).toBe(1);
     expect(pathsIntersect(new Path([0, 0], [10, 0]), new Path([0, 1], [0, 10]))).toBe(0);
--- src/roboCleaner.ts
+++ src/roboCleaner.ts
@@ -1,37 +1,37 @@
 import { Path } from "./path.class";
 
 export function roboCleaner(n: number, coords: [number, number], steps: string[]) {
-    let currentPosition = coords,
-        paths: Path[] = [],
-        cleanedArea = 0,
-        stepVectors = steps.map(el => getStepVector(el)),
-        newPosition: [number, number],
-        newPath: Path;
-    for (let i = 0; i < n; i++){
-        newPosition = vectorSum(currentPosition, stepVectors[i]);
-        newPath = new Path(currentPosition, newPosition);
-        cleanedArea += newPath.length - paths.reduce((sum, el) => sum + el.intersects(newPath), 0);
-        paths.push(newPath);
-        currentPosition = newPosition;
-    }
-    return  cleanedArea;
+  let currentPosition = coords,
+      paths: Path[] = [],
+      cleanedArea = 0,
+      stepVectors = steps.map(el => getStepVector(el)),
+      newPosition: [number, number],
+      newPath: Path;
+  for (let i = 0; i < n; i++){
+    newPosition = vectorSum(currentPosition, stepVectors[i]);
+    newPath = new Path(currentPosition, newPosition);
+    cleanedArea += newPath.length - paths.reduce((sum, el) => sum + el.intersects(newPath), 0);
+    paths.push(newPath);
+    currentPosition = newPosition;
+  }
+  return  cleanedArea;
 }
 
 function getStepVector(step: string) {
-    let s = step.split(' '),
-        direction = s[0],
-        amount = +s[1],
-        stepVector = [
-            (direction === 'W' && -amount) || (direction === 'E' && amount) || 0,
-            (direction === 'N' && -amount) || (direction === 'S' && amount) || 0
-        ]
-    return stepVector;
+  let s = step.split(' '),
+    direction = s[0],
+    amount = +s[1],
+    stepVector = [
+      (direction === 'W' && -amount) || (direction === 'E' && amount) || 0,
+      (direction === 'N' && -amount) || (direction === 'S' && amount) || 0
+    ]
+  return stepVector;
 }
 
 function vectorSum(a: number[], b: number[]): [number, number] {
-    return [a[0] + b[0], a[1] + b[1]];
+  return [a[0] + b[0], a[1] + b[1]];
 }
 
 export function pathsIntersect(pathA: Path, pathB: Path): number {
-    return pathA.intersects(pathB);
+  return pathA.intersects(pathB);
 }
