19f6ee5ada46b8439434c78b45d475bfdb829728
Anna Loginova <space@Annas-MacBook-Air.local>
2018-09-23 23:57:03 +0200

    passed overlay tests and complex path tests

--- src/roboCleaner.ts
+++ src/roboCleaner.ts
@@ -6,15 +6,54 @@ export function roboCleaner(n: number, coords: [number, number], steps: string[]
       cleanedArea = 0,
       stepVectors = steps.map(el => getStepVector(el)),
       newPosition: [number, number],
-      newPath: Path;
+      newPath: Path,
+      newCleaned: number;
   for (let i = 0; i < n; i++){
     newPosition = vectorSum(currentPosition, stepVectors[i]);
     newPath = new Path(currentPosition, newPosition);
-    cleanedArea += newPath.length - paths.reduce((sum, el) => sum + el.intersects(newPath), 0);
-    paths.push(newPath);
+    [newCleaned, paths] = merge(newPath, paths);
+    cleanedArea += newCleaned;
     currentPosition = newPosition;
   }
   return  cleanedArea;
+}
+
+
+function merge(newPath: Path, paths: Path[]): [number, Path[]] {
+  let mergedPath = newPath,
+      onLine: Path[] = [],
+      perpendicular: Path[] = [],
+      addedLength = newPath.length;
+  paths.forEach(p => {
+      let intL = newPath.intersects(p);
+      if (intL > 1 || (intL === 1 && newPath.onSameLine(p))) {
+          onLine.push(p);
+          addedLength = addedLength - intL;
+          mergedPath = mergedPath.extend(p);
+      } else if(intL === 1) {
+          perpendicular.push(p);
+      }
+  });
+  addedLength = addedLength - perpendicular.filter(el => {
+      for (let i = 0; i < onLine.length; i++) {
+          if (el.intersects(onLine[i])) {
+              return false;
+          }
+      }
+      return true;
+  }).length;
+
+  function getMergedPaths(ps: Path[], ol: Path[], mp: Path): Path[] {
+      let newPs: Path[] = [];
+      ps.forEach(p => {
+          if (ol.indexOf(p) === -1) {
+              newPs.push(p);
+          }
+      });
+      newPs.push(mp);
+      return newPs;
+  }
+  return [addedLength, getMergedPaths(paths, onLine, mergedPath)];
 }
 
 function getStepVector(step: string) {
